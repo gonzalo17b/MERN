d02e14328416ad9b71f3c22bc3934a1aea18f4aa
Gonzalo Bermejo Chacon <gbermejo@xt.mediaset.es>
2020-11-06 13:35:33 +0100

    delete wrong folders

--- Backend/dist/app/modules/shared/models/modificationNote.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ModificationNote = void 0;
-exports.ModificationNote = {
-    modified_on: Date,
-    modified_by: String,
-    modification_note: String
-};
